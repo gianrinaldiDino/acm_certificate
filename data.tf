e74e08f6293621410e435c8c2914a9d1506368b7
Gian Rinaldi <gian.rinaldi@dinocloudconsulting.com>
2024-06-10 22:21:55 -0300

    first commit.

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "route53" {
-  backend = "s3"
-  config = {
-    region   = var.region
-    bucket   = "${var.bucket_tfstate}-${var.environment}"
-    key      = "${var.region}/${var.environment}/route53.tfstate"
-  }
-}
